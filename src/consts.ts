f6e2e7fc35955cbdb3cac18fc9f4744610250839
Ksenia Nesytova <passedbyabyss@gmail.com>
2026-08-12 21:54:55 +0700

    add: tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Passed by Abyss";
+export const SITE_DESCRIPTION = "A blog about web development and design.";
